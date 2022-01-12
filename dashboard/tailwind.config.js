e4c80d75c61e7e01a4ecc16e26d9ac9f6262c81c
WaveHack <email@wavehack.net>
2022-01-12 23:29:19 +0100

    Add Tailwind CSS

---
+++ dashboard/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+      './resources/views/**/*.blade.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
--- dashboard/webpack.mix.js
+++ dashboard/webpack.mix.js
@@ -13,5 +13,8 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
-        //
+        require('tailwindcss'),
     ]);
+
+mix.sourceMaps();
+mix.version();
