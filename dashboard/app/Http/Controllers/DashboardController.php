7275e271e93fd73e348aed5c621c9ca52d956d89
WaveHack <email@wavehack.net>
2022-01-13 00:53:53 +0100

    Working on dashboard homepage

--- dashboard/app/Http/Controllers/DashboardController.php
+++ dashboard/app/Http/Controllers/DashboardController.php
@@ -2,10 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use Docker\Docker;
+
 class DashboardController
 {
     public function __invoke()
     {
-        return view('pages.dashboard');
+        $docker = Docker::create();
+
+        $containers = $docker->containerList([
+            'all' => true,
+        ]);
+
+//        foreach ($containers as $container) {
+//            dump($container);
+//        }
+//
+//        /** @var ContainerSummaryItem $portainer */
+//        $portainer = collect($containers)
+//            ->filter(fn(ContainerSummaryItem $container) => collect($container->getNames())->contains('/portainer'))
+//            ->first();
+//
+//        $volumes = $docker->volumeList([
+//            'filters' => \json_encode([
+//                'name' => ['portainer_data'],
+//            ]),
+//        ]);
+//
+//        $volume = collect($volumes->getVolumes())->first();
+//
+//        dd($volume);
+//
+//        dump('restarting portainer');
+//
+//        $docker->containerRestart($portainer->getId());
+
+        return view('pages.dashboard', compact('containers'));
     }
 }
--- dashboard/resources/views/pages/dashboard.blade.php
+++ dashboard/resources/views/pages/dashboard.blade.php
@@ -1,5 +1,35 @@
 <x-app-layout>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    <div class="text-3xl mb-2">Containers:</div>
+    <div class="flex">
+        @foreach ($containers as $container)
+            @php
+            switch ($container->getState()) {
+                case 'running':
+                    $backgroundColor = 'bg-green-200';
+                    $textColor = 'text-green-900';
+                    $dotColor = 'bg-green-500';
+                    break;
+                case 'exited':
+                    $backgroundColor = 'bg-red-200';
+                    $textColor = 'text-red-900';
+                    $dotColor = 'bg-red-500';
+                    break;
+                default:
+                    $backgroundColor = 'bg-gray-200';
+                    $textColor = 'text-gray-900';
+                    $dotColor = 'bg-gray-500';
+                    break;
+            }
+            @endphp
+            <div class="flex ml-3 items-center first:ml-0 px-2 py-1 rounded-md font-bold {{ $backgroundColor }}">
+                <div class="w-2 h-2 rounded-full {{ $dotColor }}"></div>
+                <div class="ml-2 {{ $textColor }}">
+                    {{ ucwords(str_replace('-', ' ', implode(', ', array_map(fn($string) => ltrim($string, '/'), $container->getNames())))) }}
+                </div>
+
+                {{--{{ $container->getStatus() }}
+                {{ $container->getState() }}--}}
+            </div>
+        @endforeach
+    </div>
 </x-app-layout>
