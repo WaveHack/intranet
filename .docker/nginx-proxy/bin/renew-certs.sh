93908b00764568f225f173531ab14068e7d88f77
WaveHack <email@wavehack.net>
2022-08-10 21:07:42 +0200

    Update script to renew openssl certificates

--- .docker/nginx-proxy/bin/renew-certs.sh
+++ .docker/nginx-proxy/bin/renew-certs.sh
@@ -1,9 +1,32 @@
 #!/usr/bin/env sh
 
-openssl x509 -x509toreq -in ../certs/ca.crt -signkey ../certs/ca.key -out ../certs/ca.csr
-openssl x509 -req -days 365 -in ../certs/ca.csr -signkey ../certs/ca.key -out ../certs/ca.crt
+openssl x509 \
+  -x509toreq \
+  -in ../certs/ca.crt \
+  -signkey ../certs/ca.key \
+  -out ../certs/ca.csr
+
+openssl x509 \
+  -req \
+  -days 365 \
+  -in ../certs/ca.csr \
+  -signkey ../certs/ca.key \
+  -out ../certs/ca.crt
+
 rm ../certs/ca.csr
 
-openssl x509 -x509toreq -in ../certs/home.arpa.crt -signkey ../certs/home.arpa.key -out ../certs/home.arpa.csr
-openssl x509 -req -days 365 -in ../certs/home.arpa.csr -signkey ../certs/home.arpa.key -out ../certs/home.arpa.crt
+openssl x509 \
+  -x509toreq \
+  -in ../certs/home.arpa.crt \
+  -signkey ../certs/home.arpa.key \
+  -out ../certs/home.arpa.csr
+
+openssl x509 \
+  -req \
+  -days 365 \
+  -in ../certs/home.arpa.csr \
+  -CA ../certs/ca.crt \
+  -CAkey ../certs/ca.key \
+  -out ../certs/home.arpa.crt
+
 rm ../certs/home.arpa.csr
